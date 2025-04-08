9f3d8d6fcb49130727023366a662139fed5d95d0
mklbc <90465528+mklbc@users.noreply.github.com>
2025-04-08 21:18:11 +0300

    Add files via upload

---
+++ Lab1_OpenMP_mehmet.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <omp.h>
+
+using namespace std;
+
+int main() {
+    int numThreads = omp_get_max_threads();
+    cout << "Number of threads: " << numThreads << endl;
+
+#pragma omp parallel for
+    for (int i = 0; i < 4; i++) {
+        cout << "Thread " << omp_get_thread_num() << ": Hello world, Mehmet Kulubecioglu!!!" << endl;
+    }
+
+    return 0;
+}
